4b1b40a90ae0971ab013fe3f7062d252b41517c4
unknown <nandhinimadan20@gmail.com>
2020-09-29 18:50:33 -0700

    Server.js is working

--- server.js
+++ server.js
@@ -1,42 +1,72 @@
-const express =require("express")
+const express = require("express")
 
-const server=express()
+server.use = (express.json())
 
-server.get("/",(req,res)=>{
+server.get("/", (req, res) => {
 
-    res.json({message:"hello,World"})
+    res.json({ message: "hello,World" })
 })
 
-server.get("/users",(req,res)=>{
-    const users=db.getUsers()
+server.get("/users", (req, res) => {
+    const users = db.getUsers()
 
     res.json(users)
 })
 
-server.get("/users/:id",(req,res)=>{
-    const id=req.params.id
-    const user=db.getUserById(id)
+server.get("/users/:id", (req, res) => {
+    const id = req.params.id
+    const user = db.getUserById(id)
 
-    if(user){
+    if (user) {
         res.json(user)
     }
-    else{
+    else {
         res.status(404).json(
             {
-                message:"user not found",
-            }
-            
-        )
+                message: "user not found",
+            })
     }
 })
 
-server.post("/users",(req,res)=>{
-    const newUser=db.CreateUser({
-        name:"BOB Doe",
+server.post("/users", (req, res) => {
+    const newUser = db.CreateUser({
+        name: req.body.name,
     })
     res.status(201).json(newUser)
 })
 
-server.listen(8081, () =>{
+server.put("/users/:id", (req, res) => {
+    const id=req.params.id
+    const user=db.getUserById(id)
+    if(user){
+        const updatedUser=db.updateUser(id,{
+            name:req.body.name,
+        })
+        res.json(updatedUser)
+    }
+    else{
+        res.status(404).json({
+            message:"user not found"
+        })
+    }
+
+
+})
+server.delete("/users/:id", (req, res) => {
+    const id=req.params.id
+    const user=db.getUserById(id)
+
+    if(user){
+        db.deleteUser(id)
+        res.staus(204).end()
+    }
+    else{
+        res.status(404).json({
+            message:"User not found",
+        })
+    }
+
+})
+server.listen(8081, () => {
     console.log("server started")
 })
